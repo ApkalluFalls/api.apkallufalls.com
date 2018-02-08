4a7e3fb4068bc6dd3500a9609bf157aa853eb2dd
Donnelly, James <James.Donnelly@bottomline.com>
2018-02-08 16:01:39 +0000

    Handled start city achievements.

--- scripts/achievements/_isAvailable.js
+++ scripts/achievements/_isAvailable.js
@@ -3,7 +3,6 @@
 // This function determines if an achievement is obtainable.
 module.exports = function(achievement) {
   const category = achievement["achievement_category"];
-  let reason;
 
   switch (category) {
     case 0:
@@ -21,8 +20,20 @@ module.exports = function(achievement) {
     case 60:
     case 61:
       return "legacy";
-
-    default:
-      return;
   }
+
+  switch (achievement.id) {
+    case 310:
+    case 311:
+    case 312:
+      /* These achievements are special in that once you unlock one, you are no
+       * longer able to unlock the others. Legacy characters who started in
+       * a different city to where they started when A Realm Reborn was
+       * launched can have two of these. For those reasons, this achievement is
+       * essentially ignored by Apkallu Falls.
+       */
+      return "start-city";
+  }
+
+  return undefined;
 }

5b185de8e1b3297c29fe8102b0f6b4039873177e
James Donnelly <jdonnelly30@gmail.com>
2018-02-10 17:24:22 +0000

    Supported different gender titles.

--- scripts/titles/list.js
+++ scripts/titles/list.js
@@ -3,18 +3,42 @@ const createList = require('../_list');
 
 module.exports = new Helper("Patch", "patches", {
   api: 'title',
+  columns: [
+    'id',
+    'name_de',
+    'name_en',
+    'name_fr',
+    'name_ja',
+    'name_female_de',
+    'name_female_en',
+    'name_female_fr',
+    'name_female_ja',
+    'is_prefix'
+  ],
   list: true,
   format: (data) => {
     return {
-      data: data.map(entry => ({
-        id: entry.id,
-        name: {
-          de: entry.name_de,
-          en: entry.name_en,
-          fr: entry.name_fr,
-          jp: entry.name_ja
+      data: data.map(entry => {
+        const response = {
+          id: entry.id,
+          name: {
+            de: entry.name_de,
+            en: entry.name_en,
+            fr: entry.name_fr,
+            jp: entry.name_ja
+          }
         }
-      }))
+
+        if (entry.name_en !== entry.name_female_en)
+          response.female = {
+            de: entry.name_female_de,
+            en: entry.name_female_en,
+            fr: entry.name_female_fr,
+            jp: entry.name_female_ja
+          }
+
+        return response;
+      })
     }
   }
 }, (data, base, _helperCreateJSONFn) => {

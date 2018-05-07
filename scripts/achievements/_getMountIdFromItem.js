9ef593303afac5b06814269d5676d0d9c2cca037
James Donnelly <jdonnelly30@gmail.com>
2018-05-07 23:38:52 +0100

    Added method to find mount ids from item categories.

--- scripts/achievements/_getMountIdFromItem.js
+++ scripts/achievements/_getMountIdFromItem.js
@@ -2,22 +2,43 @@
 
 // This function returns the weight of a given achievement.
 module.exports = function(mountsList, item) {
-  const itemName = item.name_en.toLowerCase();
-  const attempt1 = mountsList.filter(m => m.name_en.toLowerCase() === itemName);
+  let itemName = item.name_en.toLowerCase();
+
+  if (/barding/.test(itemName))
+    return undefined;
+
+  // Filtering out obscure matches.
+  switch (itemName) {
+    case 'gloria ignition key':
+      itemName = 'gloria-class airship';
+      break;
+    
+    case 'panther bell':
+      itemName = 'war panther';
+      break;
+  }
+
+  const attempt1 = mountsList.filter(m => m.name_en.toLowerCase() === itemName)[0];
   
   if (attempt1)
     return attempt1.id;
   
-  const attempt2 = mountsList.filter(m => m.name_en.toLowerCase() === itemName.replace(/ \w+$/, ''));
+  const attempt2 = mountsList.filter(m => m.name_en.toLowerCase() === itemName.replace(/ \w+$/, ''))[0];
 
   if (attempt2)
     return attempt2.id;
 
   // Identification Keys (for Magitek Armor)
-  const attempt3 = mountsList.filter(m => m.name_en.toLowerCase() === itemName.replace('armor identification key', 'magitek armor'));
+  const attempt3 = mountsList.filter(m => m.name_en.toLowerCase() === itemName.replace('armor identification key', 'magitek armor'))[0];
 
   if (attempt3)
     return attempt3.id;
 
-  console.info(itemName);
+  // Identification Keys (for mounts like Logistic System)
+  const attempt4 = mountsList.filter(m => m.name_en.toLowerCase() === itemName.replace(' identification key', ''))[0];
+
+  if (attempt4)
+    return attempt4.id;
+
+  throw new Error('Possible mount item? "' + item.name_en + '"');
 }

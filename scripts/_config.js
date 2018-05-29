977795b02c452fd9543eadc645e062b0e3c3f9c1
Donnelly, James <James.Donnelly@bottomline.com>
2018-05-29 10:24:44 +0100

    Updated emotes to include description, id and icon.

--- scripts/_config.js
+++ scripts/_config.js
@@ -1,4 +1,5 @@
 module.exports = {
   fullImagePath: "https:\/\/secure.xivdb.com\/img\/game\/",
+  fullImagePathV3: "https:\/\/xivdb-staging.com",
   noIcon: "https:\/\/secure.xivdb.com\/img\/ui\/noicon.png"
 }
--- scripts/emotes/dataV3.js
+++ scripts/emotes/dataV3.js
@@ -25,10 +25,26 @@ module.exports = new Helper(name, plural, {
       api: api + '/' + entry.ID,
       base,
       format: (data) => {
+        const targeted = data.content.LogMessage_Targeted;
+        const textCommand = data.content.TextCommand;
         const untargeted = data.content.LogMessage_Untargeted;
         
         return {
-          untargeted: untargeted ? parseUntargetedString(untargeted.Text_en, data.content.ID) : undefined
+          info: {
+            de: textCommand.Description_de,
+            en: textCommand.Description_en,
+            fr: textCommand.Description_fr,
+            jp: textCommand.Description_ja
+          },
+          id: data.id,
+          img: (() => {
+            if (!data.icon)
+              return false;
+            return data.icon.replace(config.fullImagePathV3, "");
+          })(),
+          // targeted: targeted ? parseTargetedString(targeted.Text_en, data.content.ID) : undefined,
+          untargeted: untargeted ? parseUntargetedString(untargeted.Text_en, data.content.ID) : undefined,
+          xivdb: data.content.url
         }
       }
     }
@@ -37,11 +53,17 @@ module.exports = new Helper(name, plural, {
   }
 });
 
+function parseTargetedString(string, id) {
+  console.info("---- Emote " + id + " did not match any known format for targeted strings.");
+  throw new Error();
+}
+
 function parseUntargetedString(string, id) {
   const end = string.match(/ ?(\w+ ?)*[.!]?$/)[0];
   let rest = string
     .replace(end, '')
     .replace('<SheetEn(ObjStr,2,PlayerParameter(7),1,1)/>', '')
+    .replace('<SheetEn(ObjStr,2,PlayerParameter(7),2,1)/>', '')
     .replace(/PlayerParameter\(7\)/g, 'CHARACTER')
     .replace(/[<(,/)>]|Clickable|If|Equal|ObjectParameter\(\w\)|PlayerParameter\(\w\)/g, '');
 
@@ -69,53 +91,53 @@ function parseUntargetedString(string, id) {
 
     if (str[0] instanceof Array && str[0].length === 6 && str[0][4].split(' her').length === 2) {
       const middleAlt = str[0][4].split(' her')[0];
-      return [
-        str[0][0] + middle + str[0][3] + end,
-        str[0][1] + ' ' + middleAlt + ' ' + 'her' + end,
-        str[0][1] + ' ' + middleAlt + ' ' + str[0][5] + end
-      ]
+      return {
+        female: str[0][1] + ' ' + middleAlt + ' ' + 'her' + end,
+        male: str[0][1] + ' ' + middleAlt + ' ' + str[0][5] + end,
+        self: str[0][0] + middle + str[0][3] + end
+      }
     }
 
     if (str[0] instanceof Array && str[0].length === 6 && str[0][4].split(' herself').length === 2) {
       const middleAlt = str[0][4].split(' herself')[0];
-      return [
-        str[0][0] + middle + str[0][3] + end,
-        str[0][1] + ' ' + middleAlt + ' ' + 'herself' + end,
-        str[0][1] + ' ' + middleAlt + ' ' + str[0][5] + end
-      ]
+      return {
+        female: str[0][1] + ' ' + middleAlt + ' ' + 'herself' + end,
+        male: str[0][1] + ' ' + middleAlt + ' ' + str[0][5] + end,
+        self: str[0][0] + middle + str[0][3] + end
+      }
     }
 
     if (str[0] instanceof Array && str[0].length === 6 && str[0][4].split(' her').length === 3) {
-      const middleAlt = str[0][4].split(' her ');
-      return [
-        str[0][0] + middle + str[0][3] + end,
-        str[0][1] + ' ' + middleAlt[0] + ' her' + middleAlt[1] + ' her' + end,
-        str[0][1] + ' ' + middleAlt[0] + ' his' + middleAlt[1] + ' him' + end
-      ]
+      const middleAlt = str[0][4].split(' her');
+      return {
+        female: str[0][1] + ' ' + middleAlt[0] + ' her' + middleAlt[1] + ' her' + end,
+        male: str[0][1] + ' ' + middleAlt[0] + ' his' + middleAlt[1] + ' him' + end,
+        self: str[0][0] + middle + str[0][3] + end
+      }
     }
 
     if (str[0] instanceof Array && str[0].length === 6) {
-      return [
-        str[0][0] + middle + str[0][3] + end,
-        str[0][1] + middle + str[0][4] + end,
-        str[0][1] + middle + str[0][5] + end
-      ]
+      return {
+        female: str[0][1] + middle + str[0][4] + end,
+        male: str[0][1] + middle + str[0][5] + end,
+        self: str[0][0] + middle + str[0][3] + end
+      }
     }
   }
 
   if (str.length === 2) {
     if (str[0] instanceof Array && str[1] instanceof Array && str[0].length === 2 && str[1].length === 2)
-      return [
-        str[0][0] + ' ' + str[1][0] + end,
-        str[0][1] + ' ' + str[1][1] + end
-      ];
+      return {
+        self: str[0][0] + ' ' + str[1][0] + end,
+        unisex: str[0][1] + ' ' + str[1][1] + end
+      };
 
     if (str[0] instanceof Array && str[1] instanceof Array && str[0].length === 2 && str[1].length === 3)
-      return [
-        str[0][0] + ' ' + str[1][0] + middle + str[1][2] + end,
-        str[0][1] + ' ' + str[1][1] + middle + str[1][2] + end,
-        str[0][1] + ' ' + str[1][1] + middle + str[1][2] + end
-      ]
+      return {
+        female: str[0][1] + ' ' + str[1][1] + middle + str[1][2] + end,
+        male: str[0][1] + ' ' + str[1][1] + middle + str[1][2] + end,
+        self: str[0][0] + ' ' + str[1][0] + middle + str[1][2] + end
+      }
 
     if (str[0] instanceof Array && str[1] instanceof Array && str[0].length === 2 && str[1].length === 4 && str[1][1].split(' your').length === 2) {
       const middleAlt = str[1][1].split(' your')[0];
@@ -127,13 +149,13 @@ function parseUntargetedString(string, id) {
     }
 
     if (str[0] instanceof Array && str[1] instanceof Array && str[0].length === 2 && str[1].length === 5)
-      return [
-        str[0][0] + ' ' + str[1][0] + middle + str[1][2] + end,
-        str[0][1] + ' ' + str[1][1] + middle + str[1][3] + end,
-        str[0][1] + ' ' + str[1][1] + middle + str[1][4] + end
-      ]
+      return {
+        female: str[0][1] + ' ' + str[1][1] + middle + str[1][3] + end,
+        male: str[0][1] + ' ' + str[1][1] + middle + str[1][4] + end,
+        self: str[0][0] + ' ' + str[1][0] + middle + str[1][2] + end
+      }
   }
 
-  console.info(id);
+  console.info("---- Emote " + id + " did not match any known format for untargeted strings.");
   throw new Error();
 }
